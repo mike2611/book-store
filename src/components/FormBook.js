d80f681e12391bad5a7e02042fa2581fd43fb5da
mike2611 <mpuentes2611@gmail.com>
2021-11-03 12:41:14 -0600

    Add submitBookToStore to FormBook.js in order to dispatch addBook

--- src/components/FormBook.js
+++ src/components/FormBook.js
@@ -1,14 +1,35 @@
+import { useDispatch } from 'react-redux';
+import uuid from 'react-uuid';
+import { addBook } from '../redux/books/booksReducer';
+
+let bookTitle = '';
+let bookAuthor = '';
+
+const updateTitle = (e) => {
+  bookTitle = e.target.value;
+};
+
+const updateAuthor = (e) => {
+  bookAuthor = e.target.value;
+};
+
+const submitBookToStore = () => {
+  const dispatch = useDispatch();
+  const newBook = {
+    id: uuid(),
+    title: bookTitle,
+    author: bookAuthor,
+  };
+  dispatch(addBook(newBook));
+};
+
 const FormBook = () => (
-  <form>
+  <div>
     <h1>ADD NEW BOOK</h1>
-    <input type="text" placeholder="Book Title" />
-    <input type="text" placeholder="Book Author" />
-    <select placeholder="Category">
-      <option>Example 1</option>
-      <option>Example 2</option>
-      <option>Example 3</option>
-    </select>
-  </form>
+    <input type="text" placeholder="Book Title" onChange={updateTitle} />
+    <input type="text" placeholder="Book Author" onChange={updateAuthor} />
+    <button type="button" onClick={submitBookToStore}>Add Book</button>
+  </div>
 );
 
 export default (FormBook);
