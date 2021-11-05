b2bde6b8c584ced1c85367bb213dc86760a3d6a0
mike2611 <mpuentes2611@gmail.com>
2021-11-05 09:58:48 -0600

    Add style to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
 
 const NavBar = () => (
-  <nav className="navbar navbar-light bg-light">
-    <ul className="navbar-nav">
-      <li className="nav-item">
-        <Link to="/" className="nav-link">Books</Link>
+  <nav className="navbar navbar-expand-md navbar-light bg-light d-flex justify-content-center justify-content-md-start">
+    <ul className="navbar-nav d-flex align-items-baseline">
+      <li className="navbar-brand ms-md-5">
+        <h1 className="navTitle">Bookstore CMS</h1>
       </li>
-      <li className="nav-item">
+      <li className="nav-item mx-auto">
+        <Link to="/" className="nav-link navBooks">Books</Link>
+      </li>
+      <li className="nav-item mx-auto">
         <Link to="/categories" className="nav-link">Categories</Link>
       </li>
     </ul>
